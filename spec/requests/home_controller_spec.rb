8bf4eaddeed9812e583329ebdf859898aae95953
AkitaYuma <yuma.akita@smart-sou.co.jp>
2023-01-07 20:40:28 +0900

    テストを作成

---
+++ spec/requests/home_controller_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe "Home", type: :request do
+  describe 'GET root' do
+    let(:user) { create(:user) }
+
+    context 'ログインしていないとき' do
+      it 'アクセスできないこと' do
+        get root_path
+        expect(response).to have_http_status(400)
+      end
+    end
+
+    context 'ログインしているとき' do
+      before do
+        sign_in user
+      end
+      it 'アクセスできること' do
+        get root_path
+        expect(response).to have_http_status(200)
+      end
+    end
+  end
+end
