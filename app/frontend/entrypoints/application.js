0a742fb1af6ff5265d5737744f46edc3a9024be7
AkitaYuma <yuma.akita@smart-sou.co.jp>
2023-01-28 23:43:48 +0900

    vueコンポーネントを作成

--- app/frontend/entrypoints/application.js
+++ app/frontend/entrypoints/application.js
@@ -1,28 +1,11 @@
-// To see this message, add the following to the `<head>` section in your
-// views/layouts/application.html.erb
-//
-//    <%= vite_client_tag %>
-//    <%= vite_javascript_tag 'application' %>
-console.log('Vite ⚡️ Rails')
+import { createApp } from 'vue'
+import App from '../javascript/app.vue'
 
-// If using a TypeScript entrypoint file:
-//     <%= vite_typescript_tag 'application' %>
-//
-// If you want to use .jsx or .tsx, add the extension:
-//     <%= vite_javascript_tag 'application.jsx' %>
+document.addEventListener('DOMContentLoaded', () => {
+  createApp(App).mount('#app')
+})
 
-console.log('Visit the guide for more information: ', 'https://vite-ruby.netlify.app/guide/rails')
-
-// Example: Load Rails libraries in Vite.
-//
-// import * as Turbo from '@hotwired/turbo'
-// Turbo.start()
-//
+// ↓active_storageがjsでつかえるようになるのか？！？！
 // import ActiveStorage from '@rails/activestorage'
 // ActiveStorage.start()
-//
-// // Import all channels.
-// const channels = import.meta.globEager('./**/*_channel.js')
 
-// Example: Import a stylesheet in app/frontend/index.css
-// import '~/index.css'
---
+++ app/frontend/javascript/app.vue
@@ -0,0 +1,28 @@
+<template>
+  <p>{{ state.message }}</p>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'App',
+  setup() {
+    const message = ref({
+      todo: []
+    })
+    return {
+      state: {
+        message: "Hello Vue3 + TypeScript!"
+      }
+    }
+  }
+})
+</script>
+
+<style scoped>
+p {
+  font-size: 2em;
+  text-align: center;
+}
+</style>
